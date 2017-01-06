ce7e291ee395f211c69197afe7bfa44f9964038b
2012saru <asd03605@gmail.com>
2017-01-06 00:27:21 -0500

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 -- Configuration
 function love.conf(t)
-	t.title = "Move Sprite From A To B In X Second"
+	t.title = "Move Image From A To B In X Second"
 	-- t.version = "0.10.2"
 	t.window.width = 800
 	t.window.height = 480
